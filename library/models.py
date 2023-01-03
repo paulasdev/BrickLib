bbaa12760070076ac23eca1fa862271b9d99c1ef
Paula da Silva <carolpaulag@gmail.com>
2023-01-03 00:02:30 +0000

    Creating database models

--- library/models.py
+++ library/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+
+class bricks (models.Model):
+    id = models.ForeignKey(User, on_delete=models.CASCADE)
+    pcs = models.IntegerField(blank=True, null=True)
+    description = models.CharField(max_length=200)
+    is_built = models.BooleanField(default=False)
+    set_number = models.IntegerField(default=0)
+   
+   
+def __str__(self):
+    return self.set_number
+    
+
+def was_published_recently(self):
+    return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
