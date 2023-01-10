203f1edef135d44225008becdf0081814b4e0df1
Paula da Silva <carolpaulag@gmail.com>
2023-01-10 02:27:09 +0000

    Add index.html on templates folder

--- library/models.py
+++ library/models.py
@@ -1,19 +1,19 @@
-import datetime
+# import datetime
 
-from django.db import models
-from django.utils.db import models
-from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
+# from django.db import models
+# from django.utils.db import models
+# from django.contrib.auth.models import User
+# from cloudinary.models import CloudinaryField
 
 
-class bricks (models.Model):
-    id = models.ForeignKey(User, on_delete=models.CASCADE)
-    pcs = models.IntegerField(blank=True, null=True)
-    description = models.CharField(max_length=200)
-    is_built = models.BooleanField(default=False)
-    set_number = models.IntegerField(default=0)
+# class bricks (models.Model):
+#     id = models.ForeignKey(User, on_delete=models.CASCADE)
+#     pcs = models.IntegerField(blank=True, null=True)
+#     description = models.CharField(max_length=200)
+#     is_built = models.BooleanField(default=False)
+#     set_number = models.IntegerField(default=0)
    
    
-def __str__(self):
-    return self.set_number
+# def __str__(self):
+#     return self.set_number
     
