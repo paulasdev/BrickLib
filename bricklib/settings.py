13dd50d11717e86ccdd38e1dece2d3a83b09d9a4
Paula da Silva <carolpaulag@gmail.com>
2023-01-11 21:09:03 +0000

    change Debug to use env varible

--- bricklib/settings.py
+++ bricklib/settings.py
@@ -29,8 +29,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
+
 
 ALLOWED_HOSTS = ["bricklib.herokuapp.com", "localhost"]
 
