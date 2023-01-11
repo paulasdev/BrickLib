d6e37c3683bf3efc2daa2552a71f10089ccebd2f
Paula da Silva <carolpaulag@gmail.com>
2023-01-11 16:32:26 +0000

    Creating the first View

--- bricklib/urls.py
+++ bricklib/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from library.views import home
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path('', home, name='home')
+    path('', include('library.urls'), name='library_urls'),
 ]
--- library/views.py
+++ library/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Set
 
-# Create your views here.
 
-
-def home(request):
-    return render(request, 'index.html/')
+class SetListView(generic.ListView):
+    model = Set
+    template_name = 'index.html'
+    paginate_by = 4
