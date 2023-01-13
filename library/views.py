a1ebc9af8f543ddfddeedf980b155adb5bb70983
Paula da Silva <carolpaulag@gmail.com>
2023-01-13 12:31:00 +0000

    creating search function

--- library/views.py
+++ library/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Set
 
 
@@ -7,3 +7,15 @@ class SetListView(generic.ListView):
     model = Set
     template_name = 'index.html'
     paginate_by = 4
+
+
+def search(request):
+    """ search function  """
+    if request.method == "POST":
+        searched = request.POST['searched']
+        sets = Set.objects.filter(theme__contains=searched)
+
+        return render(request, 'search.html', {'searched': searched, 
+        'sets': sets})
+    else: 
+        return render(request, 'search.html', {})
