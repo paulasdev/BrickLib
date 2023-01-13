16d2fcb7b586dddbe293a107d15cff05bc3fb114
Paula da Silva <carolpaulag@gmail.com>
2023-01-13 00:38:15 +0000

    add display list

--- library/admin.py
+++ library/admin.py
@@ -4,6 +4,6 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Set)
 class SetAdmin(SummernoteModelAdmin):
-
+    list_display = ('theme', 'pcs', 'set_number', 'is_built')
     search_fields = ['theme', 'pcs', 'set_number']
     summernote_fields = ('description')
