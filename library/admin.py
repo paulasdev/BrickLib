e266a79d22541e44921d9f3bc4dabb4a6dec625f
Paula da Silva <carolpaulag@gmail.com>
2023-01-14 16:53:51 +0000

    Update de fields names

--- library/admin.py
+++ library/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from .models import Set
 from django_summernote.admin import SummernoteModelAdmin
 
+
 @admin.register(Set)
-class SetAdmin(SummernoteModelAdmin):
-    list_display = ('theme', 'pcs', 'set_number', 'is_built')
-    search_fields = ['theme', 'pcs', 'set_number']
-    summernote_fields = ('description')
+class SetAdmin(admin.ModelAdmin):
+    list_display = ('theme', 'name', 'description', 'done')
+    search_fields = ('theme', 'name', 'description')
+    # summernote_fields = ('description')
