2ff16575754e9a90604f49e81a5c4a9975b8efb2
Paula da Silva <carolpaulag@gmail.com>
2023-01-14 16:58:23 +0000

    Adding the add_set to views.py

--- library/views.py
+++ library/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Set
+from .forms import SetForm
 
 
 class SetListView(generic.ListView):
@@ -16,6 +18,22 @@ def search(request):
         sets = Set.objects.filter(theme__contains=searched)
 
         return render(request, 'search.html', {'searched': searched, 
-        'sets': sets})
+                      'sets': sets})
     else: 
         return render(request, 'search.html', {})
+
+
+def add_set(request):
+    submitted = False
+    if request.method == "POST":
+        form = SetForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return HttpResponseRedirect('/add_set?submitted=True')
+    else:
+        form = SetForm
+        if 'submitted' in request.GET:
+            submitted = True
+            
+    return render(request, 'add_set.html', {'form': form, 
+                'submitted': submitted})
