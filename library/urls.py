2d0aaef20a55b4a1f116ea650b9a311ee8e943d8
Paula da Silva <carolpaulag@gmail.com>
2023-01-15 00:41:41 +0000

    adding function set_list

--- library/urls.py
+++ library/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.SetListView.as_view(), name='home'),
     path('search', views.search, name='search_results'),
     path('add_set', views.add_set, name='add_set'),
+    path('set_list', views.set_list, name='set_list'),
 ]
