49273c71135a22c48f347ba71c92a899a7ab596d
Paula da Silva <carolpaulag@gmail.com>
2023-01-16 00:39:06 +0000

    Updanting the choices options

--- library/models.py
+++ library/models.py
@@ -6,16 +6,16 @@ from cloudinary.models import CloudinaryField
 
 
 THEME_CHOICES = (
-    ('architecture', 'ARCHITECTURE'),
-    ('city', 'CITY'),
-    ('classic', 'CLASSIC'),
-    ('creator', 'CREATOR'),
-    ('friends', 'FRIENDS'), 
-    ('ideas', 'IDEAS'),
-    ('art', 'ART'),
-    ('education', 'EDUCATION'),
-    ('mario', 'MARIO'),
-    ('spider-man', 'SPIDER_MAN'),
+    ('Architecture', 'ARCHITECTURE'),
+    ('City', 'CITY'),
+    ('Classic', 'CLASSIC'),
+    ('Creator', 'CREATOR'),
+    ('Friends', 'FRIENDS'), 
+    ('Ideas', 'IDEAS'),
+    ('Art', 'ART'),
+    ('Education', 'EDUCATION'),
+    ('Mario', 'MARIO'),
+    ('Spider-man', 'SPIDER_MAN'),
 )
 def_image = "https://res.cloudinary.com/drs3eqnpf/image/upload/v1673709187/BrickLib/default_zp429f.jpg"
 
@@ -28,22 +28,21 @@ class Set (models.Model):
         editable=False)
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
-                               null=True,
-                               blank=True,
-                               related_name='user_sets')
-    name = models.CharField(max_length=50, blank=False, null=True)
+                               related_name="user_sets")
+    title = models.SlugField(max_length=50, null=True)
     theme = models.CharField(max_length=12, choices=THEME_CHOICES, blank=True)
-    description = models.CharField(max_length=300, blank=True, null=True)
+    description = models.TextField()
     done = models.BooleanField(default=False)
-    featured_image = CloudinaryField('image', default=def_image)
-    likes = models.ManyToManyField(User, related_name='set_likes', blank=True)
+    featured_image = CloudinaryField('image', default='placeholder')
     created_on = models.DateTimeField(auto_now=True)
     
 
+class Meta: 
+    ordering = ['-created_on']
+
+
 def __str__(self):
-    return self.theme
+    return self.name
    
    
-def number_of_likes(self):
-    return self.likes.count()
     
